18ad20970e35a8657a450bb2916707ddfe9aacac
ali <samzhab@gmail.com>
2021-03-22 22:39:50 -0700

    edit gemfile updating rubocop dependencies | remove gem lock file | fix year crawling error | rubocop

--- Cap_Crawl.rb
+++ Cap_Crawl.rb
@@ -1,24 +1,27 @@
 #!/usr/bin/env ruby
-require "json"
-require "addressable"
-require "rest-client"
-require "byebug"
-require "nokogiri"
-require "csv"
-require "down"
+# frozen_string_literal: true
+
+# Copyright (c) 2021 Samuel Y. Ayele
+require 'json'
+require 'addressable'
+require 'rest-client'
+require 'byebug'
+require 'nokogiri'
+require 'csv'
+require 'down'
 # require "webmock"
-require "fileutils"
+require 'fileutils'
 # url = "#{base_url}#{acts_uri}Y.html"
 
 # include WebMock::API
 # WebMock.enable!
 # ------------------------------------------------------------------------------
 class CapCrawl
-  LETTERS = %w[A B C D E F G H I J K
-               L M N O P Q R S T U V W X Y Z].freeze
+  LETTERS = %w(A B C D E F G H I J K
+               L M N O P Q R S T U V W X Y Z).freeze
 
-  BASE_URL = "https://laws-lois.justice.gc.ca/".freeze
-  ACTS_URL = "eng/acts/".freeze
+  BASE_URL = 'https://laws-lois.justice.gc.ca/'
+  ACTS_URL = 'eng/acts/'
 
   def start
     all_acts = []
@@ -26,30 +29,20 @@ class CapCrawl
       index_acts = []
       create_folders(index)
       begin
-      response = get_request("#{BASE_URL}#{ACTS_URL}#{index}.html", {})
-    rescue RestClient::ExceptionWithResponse => e
-      display_error(e, "#{BASE_URL}#{ACTS_URL}#{index}.html")
-    end
-      Nokogiri::HTML(response).css(".wet-boew-zebra").children.each do |act|
+        response = get_request("#{BASE_URL}#{ACTS_URL}#{index}.html", {})
+      rescue RestClient::ExceptionWithResponse => e
+        display_error(e, "#{BASE_URL}#{ACTS_URL}#{index}.html")
+      end
+      Nokogiri::HTML(response).css('.wet-boew-zebra').children.each do |act|
         next if act.children.count < 1
+
         act_details = get_details(act)
-        update_index_acts_list(index_acts, act_details)
-        update_all_acts_list(all_acts, act_details)
+        index_acts << act_details
+        all_acts << act_details
         write_one_to_file(index_acts, index, act_details)
-        display_message(act_details, "notice")
       end
     end
     write_all_to_file(all_acts)
-  end
-
-  def update_index_acts_list(index_acts, act_details)
-    index_acts << act_details
-    index_acts
-  end
-
-  def update_all_acts_list(all_acts, act_details)
-    all_acts << act_details
-    all_acts
   end
 
   def create_folders(index)
@@ -60,7 +53,7 @@ class CapCrawl
   end
 
   def write_all_to_file(all_acts)
-    File.write("JSONs/all_parliament_acts.json",
+    File.write('JSONs/all_parliament_acts.json',
                JSON.dump(all_acts))
   end
 
@@ -70,6 +63,7 @@ class CapCrawl
     # get_xml_file(act_details, index)
     File.write("JSONs/#{index}/#{index}_parliament_acts.json",
                JSON.dump(index_acts))
+    display_message(act_details, 'notice')
   end
 
   def get_details(act)
@@ -78,17 +72,18 @@ class CapCrawl
     cyc_details = get_cyc_details(act)
     more_details = get_more_details(act)
 
-    act_details[:uri] = act.css("a").attribute("href").value.to_s
+    act_details[:uri] = act.css('a').attribute('href').value.to_s
     act_details = act_details.merge(cyc_details)
     act_details = act_details.merge(more_details)
-    display_message(act_details, "status")
+    display_message(act_details, 'status')
     act_details
   end
 
   def display_message(act_details, flag)
-    if flag == "notice"
+    case flag
+    when 'notice'
       puts "[Notice][CAP] Finished processing ... #{act_details[:name]}"
-    elsif flag == "status"
+    when 'status'
       puts "[Status][CAP] Processing #{act_details[:name]} now..."
     end
   end
@@ -98,13 +93,13 @@ class CapCrawl
   end
 
   def get_pdf_file(uri, index)
-    Down.download("#{BASE_URL}PDF/#{uri.split('/')[0]}.pdf", destination: "PDFs")
+    Down.download("#{BASE_URL}PDF/#{uri.split('/')[0]}.pdf", destination: 'PDFs')
     Process.spawn("mv PDFs/down\*.pdf PDFs/#{index}/#{uri.split('/')[0]}.pdf")
   end
 
   def get_html_file(act_details, index)
     Down.download("#{BASE_URL}#{ACTS_URL}#{act_details[:uri].split('/')[0]}/FullText.html",
-                  destination: "HTMLs/")
+                  destination: 'HTMLs/')
     Process.spawn("mv HTMLs/down\*.html HTMLs/#{index}/#{act_details[:uri].split('/')[0]}.html")
   end
 
@@ -115,20 +110,23 @@ class CapCrawl
   # end
 
   def get_cyc_details(act)
-    cyc_details = {category: "", year: "", code: ""}
+    cyc_details = { category: '', year: '', code: '' }
+    target_content2 = act.children[2].content.strip
+    target_content4 = act.children[4].content.strip
+    return cyc_details unless target_content2[/\d+/] || target_content4[/\d+/]
+
     if act.children.count < 5
-      return cyc_details unless act.children[2].content.strip[/\d+/]
-      extract_details(act.children[2].content.strip, cyc_details)
+      extract_details(target_content2, cyc_details)
     else
-      return cyc_details unless act.children[4].content.strip[/\d+/]
-      extract_details(act.children[4].content.strip, cyc_details)
+      extract_details(target_content4, cyc_details)
     end
     cyc_details
   end
 
   def extract_details(content, cyc_details)
-    if content.split(",").count < 3
+    if content.split(',').count < 3
       # S.C. 1979, c. 7, S.C. 1979, c. 7,  R.S.C. 1979, c. 7 (sometimes
+      # S.C. 1963, c. 6
       extract_one_comma_coding(cyc_details, content)
     else
       # R.S.C., 1979, c. 7, S.C., 1979, c. 7
@@ -139,33 +137,32 @@ class CapCrawl
 
   def extract_one_comma_coding(cyc_details, content)
     # S.C. 1979, c. 7, S.C. 1979, c. 7,  R.S.C. 1979, c. 7 (sometimes)
-    cyc_details[:code] = content.strip.split(",")[1].strip # eg. c. A-5, A-14
-    cyc_details[:category] = content.split(" ")[3].strip
-    cyc_details[:year] = content.split(" ")[4].strip # eg. c. A-5, A-14
+    cyc_details[:code] = content.strip.split(',')[1].strip # eg. c. A-5, A-14
+    cyc_details[:category] = content.split[3].strip # eg. R.S.C. or S.C.
+    cyc_details[:year] = content.split[4][/[0-9]+/] # eg. 1979
   end
 
   def extract_two_comma_coding(cyc_details, content)
     # R.S.C., 1979, c. 7
-    cyc_details[:category] = content.strip.split(",")[0].split("\n")[1]
-    cyc_details[:year] = content.strip.split(",")[1].strip # 2017, 2014
-    cyc_details[:code] = content.strip.split(",")[2].strip # eg. c. A-5, A-14
+    cyc_details[:category] = content.strip.split(',')[0].split("\n")[1]
+    cyc_details[:year] = content.strip.split(',')[1].strip # 2017, 2014
+    cyc_details[:code] = content.strip.split(',')[2].strip # eg. c. A-5, A-14
   end
 
   def get_more_details(act)
-    more_details = {has_regulations: false, repealed: false}
+    more_details = { has_regulations: false, repealed: false }
     more_details[:has_regulations] = true if act.children[2].content
-                                                .strip == "R"
+                                                .strip == 'R'
     more_details[:repealed] = true if act.children[0].content.strip[/Repealed/]
     more_details
   end
 
   def get_request(url, headers)
-    response = RestClient::Request.execute(method:  :get,
-                                           url:     Addressable::URI.parse(url)
+    RestClient::Request.execute(method:  :get,
+                                url:     Addressable::URI.parse(url)
                                            .normalize.to_str,
-                                           headers: headers,
-                                           timeout: 50)
-    response
+                                headers: headers,
+                                timeout: 50)
   end
 end
 
