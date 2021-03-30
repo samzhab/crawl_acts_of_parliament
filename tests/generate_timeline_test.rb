2a19552d6ee36b552e97bd501849cd52b90960b2
AbinLiveliHood <abinabraham74@gmail.com>
2021-03-30 16:40:27 +0530

    Test for timeline generator.

--- tests/generate_timeline_test.rb
+++ tests/generate_timeline_test.rb
@@ -3,7 +3,7 @@
 # Copyright (c) 2021 Samuel Y. Ayele
 require 'test/unit'
 
-require_relative '../generate_timeline'
+require_relative '../libs/generate_timeline'
 
 class GenerateTimelineTest < Test::Unit::TestCase
   def test_source_json
@@ -23,30 +23,25 @@ class GenerateTimelineTest < Test::Unit::TestCase
   def test_process_sorted_json
     sample_json = {
       '2016' => [{ 'name' => 'Canada and Taiwan Territories Tax Arrangement Act, 2016',
-                 'year' => '2016', 'code' => 'c. 13, s. 3' }],
-      '2020' => [{ 'name' => 'Canada Emergency Response Benefit Act', 'year' => '2020',
-                 'code' => 'c. 5, s. 8' }]
+                   'year' => '2016' }],
+      '2020' => [{ 'name' => 'Canada Emergency Response Benefit Act', 'year' => '2020' }]
     }
     formatted_json = { 'title' => 'Consolidated Acts of Parliament',
       'show_today' => true, 'periods' => {} }
     timeliner = GenerateTimeline.new
     response = timeliner.process_sorted_json(formatted_json, sample_json)
-    assert_expectations(response)
-  end
-
-  def assert_expectations(response)
     expected_response = { 'title' => 'Consolidated Acts of Parliament', 'show_today' => true,
       'periods' =>
           { 2010 => { 'acts' => [{
-            'name' => 'Canada and Taiwan Territories Tax Arrangement Act, 2016', 'year' => '2016',
-            'code' => 'c. 13, s. 3'
+            'name' => 'Canada and Taiwan Territories Tax Arrangement Act, 2016', 'year' => '2016'
           }] },
             2020 => { 'acts' => [{
-              'name' => 'Canada Emergency Response Benefit Act', 'year' => '2020',
-            'code' => 'c. 5, s. 8'
+              'name' => 'Canada Emergency Response Benefit Act', 'year' => '2020'
             }] } } }
+
     assert_true response == expected_response
   end
+
   # write out these tests
   # def test_create_timeline_file
 
